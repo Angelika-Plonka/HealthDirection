a64c5752d2c7660108b36b4b27945b14246a3684
Angelika Płonka <angelikaplonka89@gmail.com>
2018-02-26 03:43:46 +0100

    finished the homepage

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -8,51 +8,11 @@ use Symfony\Component\HttpFoundation\Request;
 use AppBundle\Entity\User;
 use AppBundle\Entity\Informations;
 use AppBundle\Entity\Measurements;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\IntegerType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use FOS\UserBundle\Model\UserInterface;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 class UserController extends Controller
 {
-
-    /**
-     * @Route("/", name="homepage");
-     */
-    public function homeAction(Request $request)
-    {
-        $page = "homepage";
-        return $this->render('index.html.twig', array(
-        'page' => $page
-    ));
-    }
-
-    /**
-     * @Route("/portalDescription", name="portalDescription");
-     */
-    public function portalDescriptionAction(Request $request)
-    {
-        $page = "portal";
-        return $this->render('portalDescription.html.twig', array(
-            'page' => $page
-        ));
-    }
-
-    /**
-     * @Route("/about", name="about");
-     */
-    public function aboutAction(Request $request)
-    {
-        $page = "about";
-        return $this->render('about.html.twig', array(
-            'page' => $page
-        ));
-
-    }
-
 
     /**
      * @Route("/account", name="account");
@@ -89,28 +49,28 @@ class UserController extends Controller
                 $loadDailyEnergyRequirements = $Measurement->getDailyEnergyRequirements();
             }
         }
-//        $user = $this->getUser();
-//        $username = $this->getUser();
-//        $page = "diet";
-//        $userId = $user->getId();
-//        $loadDailyEnergyRequirements = false;
-//        if($userId){
-//            $entityManager = $this->getDoctrine()->getManager();
-//            if($entityManager->getRepository(Measurements::class)->findBy(['person' => $userId])){
-//                $Measurement = $entityManager->getRepository(Measurements::class)->findBy(['person' => $userId], ['dateAdded' => 'DESC'])[0];
-//                $loadDailyEnergyRequirements = $Measurement->getDailyEnergyRequirements();
-//            }
-//        }
-
-//        if (!is_object($user) || !$user instanceof UserInterface) {
-//            throw new AccessDeniedException('This user does not have access to this section.');
-//        }
 
         return $this->render('recipes.html.twig', array(
             'username' => $user,
             'page' => $page,
             'dailyEnergy' => $loadDailyEnergyRequirements
 
+        ));
+    }
+
+    /**
+     * @Route("/userStories", name="userStories");
+     */
+    public function userStoriesAction()
+    {
+        $page = "build";
+        $user = $this->getUser();
+        if (!is_object($user) || !$user instanceof UserInterface) {
+            throw new AccessDeniedException('This user does not have access to this section.');
+        }
+
+        return $this->render('profile/userStories.html.twig', array(
+            'page' => $page
         ));
     }
 
